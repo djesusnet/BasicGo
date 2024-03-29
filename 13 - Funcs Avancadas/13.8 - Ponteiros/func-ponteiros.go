de4a53cc54b48fe4a224bc74844c57c2d3cdc9b4
djesusnet <djesus.net@gmail.com>
2024-03-29 10:18:19 -0300

    exemplos de funcoes de  ponteiros

---
+++ 13 - Funcs Avancadas/13.8 - Ponteiros/func-ponteiros.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func inverterSinal(numero int) int {
+	return numero * -1
+}
+
+func inverterSinalComPonteiro(numero *int) {
+	*numero = *numero * -1
+}
+
+func main() {
+
+	numero := 20
+	numeroInvertido := inverterSinal(numero)
+	fmt.Println(numeroInvertido)
+	fmt.Println(numero)
+
+	novoNumero := 40
+	fmt.Println(numeroInvertido)
+	inverterSinalComPonteiro(&novoNumero)
+	fmt.Println(novoNumero)
+
+}
